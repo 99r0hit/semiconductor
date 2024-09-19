4ba0b8fb813142cc9bbd2737c6c0447f2f85728d
99r0hit <91751811+99r0hit@users.noreply.github.com>
2024-09-19 11:13:12 +0530

    Delete blog1 directory

--- blog1/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const learnMoreButton = document.getElementById('learn-more');
-    
-    learnMoreButton.addEventListener('click', () => {
-        alert('Explore more about semiconductors in our detailed modules!');
-    });
-});
