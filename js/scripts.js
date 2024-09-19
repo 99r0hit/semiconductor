cb163113a3e35a9e2025938a690da9324ae0f1ab
99r0hit <91751811+99r0hit@users.noreply.github.com>
2024-09-19 11:13:40 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const learnMoreButton = document.getElementById('learn-more');
+    
+    learnMoreButton.addEventListener('click', () => {
+        alert('Explore more about semiconductors in our detailed modules!');
+    });
+});
